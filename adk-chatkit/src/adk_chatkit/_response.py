a930bbd7c5588b89f796beeff89520211480a5d6
Sachdeva, Kapil <ksachdeva17@gmail.com>
2025-10-16 14:05:06 +0000

    fix: make sure to issue ThreadItemDoneEvent when all the events are processed

--- adk-chatkit/src/adk_chatkit/_response.py
+++ adk-chatkit/src/adk_chatkit/_response.py
@@ -1,3 +1,4 @@
+import uuid
 from collections.abc import AsyncGenerator, AsyncIterator
 from datetime import datetime
 
@@ -8,6 +9,7 @@ from chatkit.types import (
     AssistantMessageContentPartTextDelta,
     AssistantMessageItem,
     ThreadItemAddedEvent,
+    ThreadItemDoneEvent,
     ThreadItemUpdated,
     ThreadMetadata,
     ThreadStreamEvent,
@@ -22,13 +24,15 @@ async def stream_agent_response(
     if adk_response is None:
         return
 
+    response_id = str(uuid.uuid4())
+
     content_index = 0
     async for event in adk_response:
         if event.content is None:
             # we need to throw item added event first
             yield ThreadItemAddedEvent(
                 item=AssistantMessageItem(
-                    id="__fake_id__",
+                    id=response_id,
                     content=[],
                     thread_id=thread.id,
                     created_at=datetime.now(),
@@ -37,7 +41,7 @@ async def stream_agent_response(
 
             # and also yield an empty part added event
             yield ThreadItemUpdated(
-                item_id="__fake_id__",
+                item_id=response_id,
                 update=AssistantMessageContentPartAdded(
                     content_index=content_index,
                     content=AssistantMessageContent(text=""),
@@ -45,6 +49,7 @@ async def stream_agent_response(
             )
         else:
             if event.content.parts:
+                text_from_final_update = ""
                 for p in event.content.parts:
                     if p.text:
                         update: AssistantMessageContentPartTextDelta | AssistantMessageContentPartDone
@@ -58,5 +63,18 @@ async def stream_agent_response(
                                 content=AssistantMessageContent(text=p.text),
                                 content_index=content_index,
                             )
+                            text_from_final_update = p.text
 
-                        yield ThreadItemUpdated(item_id="__fake_id__", update=update)
+                        yield ThreadItemUpdated(
+                            item_id=response_id,
+                            update=update,
+                        )
+
+                yield ThreadItemDoneEvent(
+                    item=AssistantMessageItem(
+                        id=response_id,
+                        content=[AssistantMessageContent(text=text_from_final_update)],
+                        thread_id=thread.id,
+                        created_at=datetime.now(),
+                    )
+                )
