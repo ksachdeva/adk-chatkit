11653f26d4f9a8950e58f5160740f5dce8f2eb89
Sachdeva, Kapil <ksachdeva17@gmail.com>
2026-06-05 13:08:37 +0000

    refactor: in _widgets.py, use model_dump with mode=json

--- adk-chatkit/src/adk_chatkit/_widgets.py
+++ adk-chatkit/src/adk_chatkit/_widgets.py
@@ -1,9 +1,7 @@
-import json
 from typing import Any
 
 from chatkit.types import WidgetItem
 
 
 def serialize_widget_item(widget: WidgetItem) -> dict[str, Any]:
-    json_dump = widget.model_dump_json(exclude_none=True)
-    return json.loads(json_dump)  # type: ignore
+    return widget.model_dump(mode="json", exclude_none=True)
